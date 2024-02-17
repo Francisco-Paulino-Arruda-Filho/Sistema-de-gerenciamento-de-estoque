cc6c002ce54f8e0e5b5e647eb8b4b8d5e841ae92
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-02-17 14:54:36 -0300

    Encriptando senhas
    Inserindo encriptação das senhas

--- SistemaEstoque/validaLogin.php
+++ SistemaEstoque/validaLogin.php
@@ -12,16 +12,28 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
+    function encriptarSenha($senha) {
+        // Encriptar a senha usando SHA-256
+        $senha_encriptada = hash('sha256', $senha);
+        return $senha_encriptada;
+    }
+
     //Chega se o usuário consta na base de dados e executa a query
-    $sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+    $sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
     $conexao = $conexao->conectar();
     $stmt = $conexao->prepare($sql);
-    
+    $stmt->bindValue(1, $email);
+    $stmt->bindValue(2, encriptarSenha($senha));
     $stmt->execute();
     
     $resultado = $stmt->fetchAll(PDO::FETCH_OBJ);
     print_r($resultado);
 
+    $sql = "SELECT * FROM usuario";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    
+    $resultado = $stmt->fetchAll(PDO::FETCH_OBJ);
     echo '<pre>';
     print_r($resultado);
     echo '<pre>';
@@ -33,8 +45,6 @@
         $_SESSION['id_usuario'] = $resultado[0]->id_usuario;
         $_SESSION['perfil_id'] = $resultado[0]->perfil_id;
         $usuario_autenticado = true;
-        /*echo $_SESSION['id'];
-        echo $_SESSION['perfil_id'];*/
         //Força o redirecionamento
         header('Location:novo_produto.php');
     } else {
